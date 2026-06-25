f44ca6fe95ea6ebe066b699cf513577865dd900d
David Whitlock <david.m.whitlock@gmail.com>
2026-06-24 18:46:19 -0700

    Completed first Koan

--- koans/src/beginner/AboutKoans.java
+++ koans/src/beginner/AboutKoans.java
@@ -2,18 +2,19 @@ package beginner;
 
 import com.sandwich.koan.Koan;
 
+import static com.sandwich.util.Assert.assertTrue;
 import static com.sandwich.util.Assert.fail;
 
 public class AboutKoans {
 
     @Koan
     public void findAboutKoansFile() {
-        fail("delete this line to advance");
+        assertTrue(true);
     }
 
     @Koan
     public void definitionOfKoanCompletion() {
-        boolean koanIsComplete = false;
+        boolean koanIsComplete = true;
         if (!koanIsComplete) {
             fail("what if koanIsComplete variable was true?");
         }
